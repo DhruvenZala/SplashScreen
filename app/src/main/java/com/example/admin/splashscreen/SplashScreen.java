4b3a75d7f41c6f6eaa064c48e295e8f2967e82ae
DhruvenZala <dhruvenzala@gmail.com>
2017-06-10 15:09:19 +0530

    Removing the Status Bar during Splash Screen

--- app/src/main/java/com/example/admin/splashscreen/SplashScreen.java
+++ app/src/main/java/com/example/admin/splashscreen/SplashScreen.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 public class SplashScreen extends AppCompatActivity {
@@ -13,6 +14,9 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Setting Full Screen
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
 
 
